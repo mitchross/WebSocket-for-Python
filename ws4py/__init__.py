2df4c496899c473e4390594890943f0c4c60d412
Mitch Ross <mitchross09@gmail.com>
2021-09-05 13:04:29 -0400

    Revert "default logs debug"
    This reverts commit ef9e3b8558f39f32f8187a51b2d854b4c3770eab.

--- ws4py/__init__.py
+++ ws4py/__init__.py
@@ -36,7 +36,7 @@ __all__ = ['WS_KEY', 'WS_VERSION', 'configure_logger', 'format_addresses']
 WS_KEY = b"258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
 WS_VERSION = (8, 13)
 
-def configure_logger(stdout=True, filepath=None, level=logging.DEBUG):
+def configure_logger(stdout=True, filepath=None, level=logging.INFO):
     logger = logging.getLogger('ws4py')
     logger.setLevel(level)
     logfmt = logging.Formatter("[%(asctime)s] %(levelname)s %(message)s")
